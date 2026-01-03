bffea7536064ef85e901b88bdc0b38f04b66aee8
Icokruger999 <ico@astutetech.co.za>
2026-01-03 12:42:16 +0200

    Fix missing using directive for Company entity

--- InsuranceLoom.Api/Controllers/CompanyController.cs
+++ InsuranceLoom.Api/Controllers/CompanyController.cs
@@ -1,5 +1,6 @@
 using InsuranceLoom.Api.Data;
 using InsuranceLoom.Api.Models.DTOs;
+using InsuranceLoom.Api.Models.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
